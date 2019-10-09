6f2c21d2ab320465a3597036179f0b6b61d7b3df
Noe Cortez <corteznoe3@gmail.com>
2019-10-09 14:29:36 -0600

    actividad Estacionamiento creada

---
+++ app/src/main/java/com/example/control_estacionamiento_utec_electiva_i/docentes/Estacionamiento.java
@@ -0,0 +1,16 @@
+package com.example.control_estacionamiento_utec_electiva_i.docentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.control_estacionamiento_utec_electiva_i.R;
+
+public class Estacionamiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estacionamiento);
+    }
+}
