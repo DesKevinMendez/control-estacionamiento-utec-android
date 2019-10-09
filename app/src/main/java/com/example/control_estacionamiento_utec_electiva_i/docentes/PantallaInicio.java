b65c154d4aff8f01bd2f9ec3e4912e08e015ea16
Noe Cortez <corteznoe3@gmail.com>
2019-10-09 14:22:35 -0600

    Se modifaron las vistas de docente y el toolbar. El toolbar se modifico de manera basica

--- app/src/main/java/com/example/control_estacionamiento_utec_electiva_i/docentes/PantallaInicio.java
+++ app/src/main/java/com/example/control_estacionamiento_utec_electiva_i/docentes/PantallaInicio.java
@@ -3,6 +3,9 @@ package com.example.control_estacionamiento_utec_electiva_i.docentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.control_estacionamiento_utec_electiva_i.R;
 
@@ -12,5 +15,27 @@ public class PantallaInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+
+    }
+
+    // Method for show and hide the menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    // Method to assign the corresponding functions to the options of menu
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.menuInicio){
+            Toast.makeText(this, "opcion 1", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.menuPerfil){
+            Toast.makeText(this, "opcion 2", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.menuCerrar){
+            Toast.makeText(this, "opcion 3", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
