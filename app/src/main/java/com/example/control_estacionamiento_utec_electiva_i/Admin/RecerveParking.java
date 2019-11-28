cdd33abf2930e787fa882b3b70a5c1a3ef72d312
Kevin Mendez <kevin_mendez30@outlook.com>
2019-11-28 13:25:41 -0600

    Se puede reservar un parqueo

--- app/src/main/java/com/example/control_estacionamiento_utec_electiva_i/Admin/RecerveParking.java
+++ app/src/main/java/com/example/control_estacionamiento_utec_electiva_i/Admin/RecerveParking.java
@@ -1,6 +1,7 @@
 package com.example.control_estacionamiento_utec_electiva_i.Admin;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.icu.util.Calendar;
 import android.net.Uri;
@@ -21,17 +22,33 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.HelpersClass.DatosBuilding;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.HelpersClass.DatosEvents;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.HelpersClass.DatosSchedule;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.HelpersClass.DatosTeacher;
+import com.example.control_estacionamiento_utec_electiva_i.Admin.HelpersClass.DatosVigilante;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.InfoSetViews.ReserveParkingDataSend;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.ViewAssignParking.SelectedBuilding;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.ViewAssignParking.SelectedSchedule;
 import com.example.control_estacionamiento_utec_electiva_i.Admin.ViewAssignParking.SelectedTeacher;
 import com.example.control_estacionamiento_utec_electiva_i.HTTP.HttpRequestAdmin;
+import com.example.control_estacionamiento_utec_electiva_i.Models.User;
 import com.example.control_estacionamiento_utec_electiva_i.R;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.control_estacionamiento_utec_electiva_i.Interfaces.Globals.BASE_URL;
 
 public class RecerveParking extends Fragment implements View.OnClickListener {
     private OnFragmentInteractionListener mListener;
@@ -197,7 +214,31 @@ public class RecerveParking extends Fragment implements View.OnClickListener {
                     Log.i("Cantida reservador", cantidadReservaEstacionamiento);
                     Log.i("Horarios", DatosSchedule.getHoraEntrada() + " " + DatosSchedule.getHoraSalida());
                     Log.i("Motivo de reserva", comentario);
-                    changeFragments(new InicioAdmin());
+
+                    String[] horaEntrada = DatosSchedule.getHoraEntrada().split(":");
+                    String[] horaSalida = DatosSchedule.getHoraSalida().split(":");
+                    String HoraEntrada = "";
+                    String HoraSalida = "";
+
+                    if (Integer.valueOf(horaEntrada[0]) <10 ){
+                        HoraEntrada = "0"+horaEntrada[0]+":"+horaEntrada[1];
+                    } else {
+                        HoraEntrada = DatosSchedule.getHoraEntrada();
+                    }
+
+                    if (Integer.valueOf(horaSalida[0])< 10){
+                        HoraSalida = "0"+horaSalida[0]+":"+horaSalida[1];
+
+                    }else {
+                        HoraSalida = DatosSchedule.getHoraSalida();
+                    }
+                    Log.i("FORMATO", HoraEntrada);
+                    Log.i("FORMATO", HoraSalida);
+
+                    HTTPrequestResereParking(DatosTeacher.getTeacherIdSelected(), DatosBuilding.getBuildingIdSelected(),
+                            fecha, HoraEntrada, HoraSalida,
+                            cantidadReservaEstacionamiento, comentario);
+
                 }
 
 
@@ -215,6 +256,62 @@ public class RecerveParking extends Fragment implements View.OnClickListener {
                     break;
         }
     }
+    ProgressDialog progressDialog;
+    public void HTTPrequestResereParking(String vigilante_id,
+                                         String edificio_id, String fecha, String entrada,
+                                         String salida, String cantidad, String motivo) {
+
+
+        progressDialog = new ProgressDialog(getContext(), R.style.AlertDialogStyle);
+        progressDialog.setMessage("Asignando vigilante...");
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(false);
+
+        progressDialog.show();
+
+        String url = BASE_URL+"reservar-parqueo?api_token="+ User.getApi_token();
+        Map<String, String> params = new HashMap();
+        params.put("user_id", String.valueOf(vigilante_id));
+        params.put("edificio_id", String.valueOf(edificio_id));
+        params.put("fecha", fecha);
+        params.put("hora_entrada", entrada);
+        params.put("hora_salida", salida);
+        params.put("cantidad", String.valueOf(cantidad));
+        params.put("comentario", motivo);
+
+        JSONObject parameters = new JSONObject(params);
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Toast.makeText(getContext(), "Reserva de parqueo asignada", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+
+                changeFragments(new InicioAdmin());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+
+            }
+        }){
+            /** Passing some request headers* */
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                return headers;
+            }
+        };
+
+        queue.add(request);
+
+    }
 
     private void showDatePickerDialog() {
         Calendar mcurrentDate = Calendar.getInstance();
@@ -226,7 +323,7 @@ public class RecerveParking extends Fragment implements View.OnClickListener {
         mDatePicker = new DatePickerDialog(getActivity(), R.style.CustomDatePickerDialogTheme, new DatePickerDialog.OnDateSetListener() {
             public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
                 TextView PlannedDate = getActivity().findViewById(R.id.edtFecha);
-                String selectedDate = selectedday + " / " + (selectedmonth+1) + " / " + selectedyear;
+                String selectedDate = selectedyear+ "-" + (selectedmonth+1) + "-" + selectedday ;
                 PlannedDate.setText(selectedDate);
                 ReserveParkingDataSend.setFechaReserva(selectedDate);
 
